91119a5a96a8ecfc85ae7567fea3eae0abaffe4b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Data;
+using DevExpress.XtraCharts;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ASPxPivotGrid1.DataSource = GetDataTable();
+
+        if (!IsPostBack)
+        {
+            WebChartControl1.SeriesTemplate.ChangeView(DevExpress.XtraCharts.ViewType.Pie   );
+            WebChartControl1.SeriesDataMember = "Series";
+            WebChartControl1.SeriesTemplate.ArgumentDataMember = "Arguments";
+            WebChartControl1.SeriesTemplate.ValueDataMembers.AddRange(new string[] { "Values" });
+            WebChartControl1.SeriesTemplate.PointOptions.ValueNumericOptions.Format = DevExpress.XtraCharts.NumericFormat.Percent;
+            WebChartControl1.SeriesTemplate.PointOptions.Pattern = "{A} - {V}";
+            WebChartControl1.CustomDrawSeriesPoint += new DevExpress.XtraCharts.CustomDrawSeriesPointEventHandler(WebChartControl1_CustomDrawSeriesPoint);
+
+        }
+        
+    }
+
+    void WebChartControl1_CustomDrawSeriesPoint(object sender, DevExpress.XtraCharts.CustomDrawSeriesPointEventArgs e)
+    {
+        ((PieDrawOptions)e.SeriesDrawOptions).FillStyle.FillMode = FillMode.Solid;
+        ((PieDrawOptions)e.LegendDrawOptions).FillStyle.FillMode = FillMode.Solid;
+        ((PieDrawOptions)e.SeriesDrawOptions).Color = Color.FromName(e.SeriesPoint.Argument);
+        ((PieDrawOptions)e.LegendDrawOptions).Color = Color.FromName(e.SeriesPoint.Argument);
+    }
+
+    private DataTable GetDataTable()
+    {
+        DataTable data = new DataTable();
+        data.Columns.Add("Name", typeof(string));
+        data.Columns.Add("OrderDate", typeof(DateTime ));
+        data.Columns.Add("ProductAmount", typeof(int));
+
+        data.Rows.Add(new object[] { "Red", DateTime.Today, 10 });
+        data.Rows.Add(new object[] { "Green", DateTime.Today.AddMonths(2), 5 });
+        data.Rows.Add(new object[] { "Blue", DateTime.Today.AddMonths(3), 7 });
+        data.Rows.Add(new object[] { "Yellow", DateTime.Today.AddMonths(4), 3 });
+
+        return data;
+
+    }
+    
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Drawing
+Imports System.Data
+Imports DevExpress.XtraCharts
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		ASPxPivotGrid1.DataSource = GetDataTable()
+
+		If (Not IsPostBack) Then
+			WebChartControl1.SeriesTemplate.ChangeView(DevExpress.XtraCharts.ViewType.Pie)
+			WebChartControl1.SeriesDataMember = "Series"
+			WebChartControl1.SeriesTemplate.ArgumentDataMember = "Arguments"
+			WebChartControl1.SeriesTemplate.ValueDataMembers.AddRange(New String() { "Values" })
+			WebChartControl1.SeriesTemplate.PointOptions.ValueNumericOptions.Format = DevExpress.XtraCharts.NumericFormat.Percent
+			WebChartControl1.SeriesTemplate.PointOptions.Pattern = "{A} - {V}"
+			AddHandler WebChartControl1.CustomDrawSeriesPoint, AddressOf WebChartControl1_CustomDrawSeriesPoint
+
+		End If
+
+	End Sub
+
+	Private Sub WebChartControl1_CustomDrawSeriesPoint(ByVal sender As Object, ByVal e As DevExpress.XtraCharts.CustomDrawSeriesPointEventArgs)
+		CType(e.SeriesDrawOptions, PieDrawOptions).FillStyle.FillMode = FillMode.Solid
+		CType(e.LegendDrawOptions, PieDrawOptions).FillStyle.FillMode = FillMode.Solid
+		CType(e.SeriesDrawOptions, PieDrawOptions).Color = Color.FromName(e.SeriesPoint.Argument)
+		CType(e.LegendDrawOptions, PieDrawOptions).Color = Color.FromName(e.SeriesPoint.Argument)
+	End Sub
+
+	Private Function GetDataTable() As DataTable
+		Dim data As New DataTable()
+		data.Columns.Add("Name", GetType(String))
+		data.Columns.Add("OrderDate", GetType(DateTime))
+		data.Columns.Add("ProductAmount", GetType(Integer))
+
+		data.Rows.Add(New Object() { "Red", DateTime.Today, 10 })
+		data.Rows.Add(New Object() { "Green", DateTime.Today.AddMonths(2), 5 })
+		data.Rows.Add(New Object() { "Blue", DateTime.Today.AddMonths(3), 7 })
+		data.Rows.Add(New Object() { "Yellow", DateTime.Today.AddMonths(4), 3 })
+
+		Return data
+
+	End Function
+
+End Class
